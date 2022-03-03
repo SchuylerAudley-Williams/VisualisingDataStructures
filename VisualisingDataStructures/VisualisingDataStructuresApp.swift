e6493540376cbc75b462c6d06058b3ac92ac044c
Schuyler Audley-Williams <audley-williams.s@etoncollege.org.uk>
2022-03-03 11:53:53 +0000

    Initial Commit

---
+++ VisualisingDataStructures/VisualisingDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructuresApp.swift
+//  VisualisingDataStructures
+//
+//  Created by Audley-Williams, Schuyler (SPH) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
